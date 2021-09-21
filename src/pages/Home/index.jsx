800386314b7c3d5db98b9e2b7e21cbfad46abe32
Igor Pauleski <igorpauleski@hotmail.com>
2021-09-20 21:32:50 -0300

    Projeto iniciado em sala, desenvolvendo as Rotas parte 2

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,16 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export const Home = () => {
+    const [produtos,getProdutos] = useState([]);
+    
+    useEffect ( () = {
+        axios.get('http://react.professorburnes.com.br').then((response) => {
+            console.log(response.data);
+        })
+    })
+
     return(
         <>
             <h1 className="text-center">Produtos em Destaque</h1>
